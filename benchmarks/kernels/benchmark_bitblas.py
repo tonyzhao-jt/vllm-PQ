1fed8395d3b444ab50c3fe4c7dc1665a4fbd7f38
LeiWang1999 <leiwang1999@outlook.com>
2024-08-21 05:22:07 +0000

    remove dep

--- benchmarks/kernels/benchmark_bitblas.py
+++ benchmarks/kernels/benchmark_bitblas.py
@@ -1,6 +1,18 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
+try:
+    import bitblas
+    if bitblas.__version__ < "0.0.1.dev14":
+        raise ImportError("bitblas version is wrong. Please "
+                            "install bitblas>=0.0.1.dev14")
+except ImportError as e:
+    bitblas_import_exception = e
+    raise ValueError(
+        "Trying to use the bitblas backend, but could not import"
+        f"with the following error: {bitblas_import_exception}"
+    ) from bitblas_import_exception
+
 from bitblas import Matmul, MatmulConfig, auto_detect_nvidia_target
 
 from vllm.utils import FlexibleArgumentParser
