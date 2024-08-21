0d364e78ee18f96583236b002e3bad1fbbaa5309
LeiWang1999 <leiwang1999@outlook.com>
2024-08-21 04:43:05 +0000

    remove all bitnet related items

--- vllm/transformers_utils/tokenizers/__init__.py
+++ vllm/transformers_utils/tokenizers/__init__.py
@@ -1,4 +1,3 @@
 from vllm.transformers_utils.tokenizers.baichuan import BaichuanTokenizer
-from vllm.transformers_utils.tokenizers.bitnet import BitnetTokenizer
 
-__all__ = ["BaichuanTokenizer", "BitnetTokenizer"]
+__all__ = ["BaichuanTokenizer"]
