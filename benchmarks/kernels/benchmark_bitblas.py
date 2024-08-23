31a7bbdaf70aaaa4202f8aba7d4f6823a318b5ee
LeiWang1999 <leiwang1999@outlook.com>
2024-08-23 07:18:48 +0000

    lint fix

--- benchmarks/kernels/benchmark_bitblas.py
+++ benchmarks/kernels/benchmark_bitblas.py
@@ -12,8 +12,7 @@ except ImportError as e:
         "Trying to use the bitblas backend, but could not import"
         f"with the following error: {bitblas_import_exception}. "
         "Please install bitblas through the following command: "
-        "`pip install bitblas>=0.0.1.dev15`"
-    ) from bitblas_import_exception
+        "`pip install bitblas>=0.0.1.dev15`") from bitblas_import_exception
 
 from bitblas import Matmul, MatmulConfig, auto_detect_nvidia_target
 
