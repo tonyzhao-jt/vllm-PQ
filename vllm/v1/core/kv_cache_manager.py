e2aa6ded4f5ed0215e0c0169868df51f03fc839c
Cody Yu <hao.yu.cody@gmail.com>
2025-01-31 19:32:33 +0000

    test
    Signed-off-by: Cody Yu <hao.yu.cody@gmail.com>

--- vllm/v1/core/kv_cache_manager.py
+++ vllm/v1/core/kv_cache_manager.py
@@ -72,8 +72,8 @@ class KVCacheManager:
         self.req_to_blocks: DefaultDict[str,
                                         List[KVCacheBlock]] = defaultdict(list)
 
-        # Prefix cache metrics.
-        self.prefix_caching_metrics = PrefixCachingMetrics()
+        # Prefix cache metrics. TODO: Make the interval configurable.
+        self.prefix_caching_metrics = PrefixCachingMetrics(interval=1000)
 
     @property
     def usage(self) -> float:
